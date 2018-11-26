22bd71fcdbe0e7ec5633df5ce4fda40dc926fd44
sumantphegde <sumantita@gmail.com>
2018-11-26 15:40:46 -0800

    UI update

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,35 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthenticateService } from './../authenticate.service'
-import { AppUser } from './../models/appUser';
-import { ShoppingCartService } from '../shopping-cart.service';
-import { ShoppingCart } from '../models/shopping-cart';
-import { Observable } from 'rxjs';
-import { AngularFireObject } from 'angularfire2/database';
+import { Component, OnInit } from "@angular/core";
+import { AuthenticateService } from "./../authenticate.service";
+import { AppUser } from "./../models/appUser";
+import { ShoppingCartService } from "../shopping-cart.service";
+import { ShoppingCart } from "../models/shopping-cart";
+import { Observable } from "rxjs";
+import { Router } from "@angular/router";
+import { AngularFireObject } from "angularfire2/database";
 
 @Component({
-  selector: 'navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"]
 })
-export class NavbarComponent implements OnInit{
+export class NavbarComponent implements OnInit {
   appUser: AppUser;
   cart$: Observable<ShoppingCart>;
-  constructor(private auth: AuthenticateService,private shoppingCartService: ShoppingCartService) {
-    
-   }
+  constructor(
+    private router: Router,
+    private auth: AuthenticateService,
+    private shoppingCartService: ShoppingCartService
+  ) {}
 
-  logout()
-  {
+  logout() {
     this.auth.logout();
+    this.router.navigate(["/"]);
   }
 
-  async ngOnInit(){
-    this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
-    this.cart$ =await this.shoppingCartService.getCart();
+  async ngOnInit() {
+    this.auth.appUser$.subscribe(appUser => (this.appUser = appUser));
+    this.cart$ = await this.shoppingCartService.getCart();
     console.log("yooo");
     console.log(this.cart$);
   }
-
-
-
 }
